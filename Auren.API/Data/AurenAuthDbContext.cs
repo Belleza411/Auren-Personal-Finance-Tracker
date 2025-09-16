efe08b9cd1ba17b52a60eb9a683671a4d1f28730
Belleza411 <aevanazelleb@gmail.com>
2025-09-16 12:13:20 +1200

    feat(data): Setup DbContext and configure ApplicationUser and RefreshToken

---
+++ Auren.API/Data/AurenAuthDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Auren.API.Models.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auren.API.Data
+{
+    public class AurenAuthDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AurenAuthDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>().HasAlternateKey(u => u.UserId);
+
+            builder.Entity<RefreshToken>(entity =>
+            {
+                entity.HasKey(rt => rt.RefreshTokenId);
+
+                entity.Property(rt => rt.Token)
+                      .IsRequired()
+                      .HasMaxLength(500);
+
+                entity.Property(rt => rt.UserId)
+                      .IsRequired();
+
+                entity.Property(rt => rt.ReplacedByToken)
+                      .HasMaxLength(500);
+
+                entity.Property(rt => rt.ReasonRevoked)
+                      .HasMaxLength(200);
+
+                entity.HasOne(rt => rt.User)
+                      .WithMany(u => u.RefreshTokens)
+                      .HasForeignKey(rt => rt.UserId)
+                      .HasPrincipalKey(u => u.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasIndex(rt => rt.Token)
+                      .IsUnique();
+
+                entity.HasIndex(rt => rt.UserId);
+
+                entity.Property(rt => rt.IsActive)
+                      .HasComputedColumnSql("CAST(CASE WHEN IsRevoked = 0 AND ExpiryDate > GETUTCDATE() THEN 1 ELSE 0 END AS BIT)");
+            });
+        }
+    }
+}
--- Auren.API/Program.cs
+++ Auren.API/Program.cs
@@ -1,3 +1,8 @@
+using Auren.API.Data;
+using Auren.API.Models.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +10,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AurenAuthDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AurenAuthDbConnection"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    // Strong password policy
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequiredUniqueChars = 4;
+
+    // Account lockout
+    options.Lockout.AllowedForNewUsers = true;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+
+    // User settings
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedEmail = false;
+})
+    .AddEntityFrameworkStores<AurenAuthDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
