e815d28d1502e724590c3b81934649ccfb24adc1
Belleza411 <aevanazelleb@gmail.com>
2025-09-18 19:11:37 +1200

    feat(auth): Added Auth Controller feat(auth): Added Register Endpoint

---
+++ Auren.API/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using Auren.API.DTOs.Requests;
+using Auren.API.DTOs.Responses;
+using Auren.API.Models.Domain;
+using Auren.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auren.API.Controllers
+{
+	[Route("api/auth")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		private readonly ITokenRepository _tokenRepository;
+		private readonly IUserRepository _userRepository;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly ILogger<AuthController> _logger;
+
+		public AuthController(ITokenRepository tokenRepository,
+			IUserRepository userRepository,
+            SignInManager<ApplicationUser> signInManager,
+			UserManager<ApplicationUser> userManager,
+			ILogger<AuthController> logger)
+		{
+			_tokenRepository = tokenRepository;
+			_userRepository = userRepository;
+            _signInManager = signInManager;
+			_userManager = userManager;
+			_logger = logger;
+		}
+
+		[HttpPost("register")]
+		public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(new AuthResponse
+				{
+					Success = false,
+					Message = "Invalid Input",
+                    Errors = ModelState.SelectMany(x => x.Value?.Errors.Select(e => e.ErrorMessage) ?? Enumerable.Empty<string>()).ToList()
+                });
+			}
+
+			var result = await _userRepository.RegisterAsync(request);
+
+			if(result.Success && result.User != null)
+			{
+				var user = await _userManager.FindByEmailAsync(request.Email);
+				if(user != null)
+				{
+					await _signInManager.SignInAsync(user, isPersistent: true);
+					return Ok(result);
+				}
+            }
+
+			return result.Success ? Ok(result) : BadRequest(result);
+        }
+    }
+}
