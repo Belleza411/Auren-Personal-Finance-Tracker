a482ae24c54b071927da945a56be71ecdfc4c207
Belleza411 <aevanazelleb@gmail.com>
2025-11-27 16:57:45 +1300

    chore: Added Result Pattern

---
+++ Auren.API/Helpers/Result/Error.cs
@@ -0,0 +1,40 @@
+﻿namespace Auren.API.Helpers.Result
+{
+	public class Error
+	{
+        public string Code { get; }
+        public List<string> Messages { get; }
+
+        public Error(string code, IEnumerable<string>? messages = null)
+        {
+            Code = code ?? string.Empty;
+            Messages = messages?.ToList() ?? new List<string>();
+        }
+
+        public static Error None => new Error(string.Empty, Enumerable.Empty<string>());
+        public static Error NotFound(params string[] messages) => new Error("NOT_FOUND", messages);
+        public static Error TypeMismatch(params string[] messages) => new Error("TYPE_MISMATCH", messages);
+        public static Error NotEnoughBalance(params string[] messages) => new Error("NOT_ENOUGH_BALANCE", messages);
+    }
+
+    public class ResultWithError
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public Error Error { get; }
+
+        protected ResultWithError(bool isSuccess, Error error)
+        {
+            if (!isSuccess && error == null)
+                throw new ArgumentNullException(nameof(error), "Failure result must have an Error instance.");
+
+            IsSuccess = isSuccess;
+            Error = isSuccess ? Error.None : error;
+        }
+
+        public static ResultWithError Success() => new ResultWithError(true, Error.None);
+        public static ResultWithError Failure(Error error) => new ResultWithError(false, error);
+        public static ResultWithError Failure(string code, params string[] messages) =>
+            new ResultWithError(false, new Error(code, messages));
+    }
+}
---
+++ Auren.API/Helpers/Result/Result.cs
@@ -0,0 +1,38 @@
+﻿namespace Auren.API.Helpers.Result
+{
+	public class Result
+	{
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public Error Error { get; }
+
+        protected Result(bool isSuccess, Error error)
+        {
+            if (!isSuccess && error == null)
+                throw new ArgumentNullException(nameof(error), "Failure result must have an Error instance.");
+            IsSuccess = isSuccess;
+            Error = IsSuccess ? Error.None : error;
+        }
+
+        public static Result Success() => new Result(true, Error.None);
+        public static Result Failure(Error error) => new Result(false, error);
+        public static Result Failure(string code, params string[] messages) =>
+            new Result(false, new Error(code, messages));
+
+        public static Result<T> Success<T>(T value) => new Result<T>(value, true, Error.None);
+        public static Result<T> Failure<T>(Error error) => new Result<T>(default!, false, error);
+        public static Result<T> Failure<T>(string code, params string[] messages) =>
+            new Result<T>(default!, false, new Error(code, messages));
+    }
+
+    public class Result<T> : Result
+    {
+        public T Value { get; }
+
+        protected internal Result(T value, bool isSuccess, Error error)
+            : base(isSuccess, error)
+        {
+            Value = value;
+        }
+    }
+}
