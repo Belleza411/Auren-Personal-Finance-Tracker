f07a1d176c3b00eee4e5e6c98f729404a121486b
Belleza411 <aevanazelleb@gmail.com>
2025-12-09 09:23:44 +1300

    fix: Fix naming convention

--- src/backend/Auren.Application/DTOs/Responses/Transaction/AvgDailySpendingResponse.cs
+++ src/backend/Auren.Application/DTOs/Responses/Transaction/AvgDailySpendingResponse.cs
@@ -1,4 +1,4 @@
 ﻿namespace Auren.Application.DTOs.Responses.Transaction
 {
-	public sealed record AvgDailySpendingResponse(decimal avgSpending, decimal pecentageChange);
+	public sealed record AvgDailySpendingResponse(decimal AvgSpending, decimal PercentageChange);
 }
