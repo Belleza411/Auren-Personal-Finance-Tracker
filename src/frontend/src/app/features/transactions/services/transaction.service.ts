604912e032f9dafabb3cc1488a4a39be69ff5947
Belleza411 <aevanazelleb@gmail.com>
2025-12-08 13:48:16 +1300

    feat: Added TransactionService to make HTTP calls and observables

---
+++ src/frontend/src/app/features/transactions/services/transaction.service.ts
@@ -0,0 +1,47 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { CreateTransaction, Transaction, TransactionFilter } from '../models/transaction.model';
+import { apiUrl } from '../../../../environments/environment';
+import { Observable} from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransactionService {
+  private readonly baseUrl = `${apiUrl}/api/transactions`;
+  private http = inject(HttpClient);
+
+  getAllTransactions(
+    filters: Partial<TransactionFilter>,
+    pageSize: number = 5,
+    pageNumber: number = 1
+  ) : Observable<Transaction[]> {
+      let params = new HttpParams()
+        .set('pageNumber', pageNumber.toString())
+        .set('pageSize', pageSize.toString());
+    
+      Object.entries(filters).forEach(([key, value]) => {
+        if (value !== undefined && value !== null) {
+          params = params.set(key, value.toString());
+        }
+      });
+
+      return this.http.get<Transaction[]>(this.baseUrl, { params });
+  }
+
+  getTransactionById(id: string): Observable<Transaction> {
+      return this.http.get<Transaction>(`${this.baseUrl}/${id}`);
+  }
+
+  createTransaction(data: CreateTransaction): Observable<Transaction> {
+    return this.http.post<Transaction>(this.baseUrl, data);
+  }
+
+  updateTransaction(id: string, data: Partial<CreateTransaction>): Observable<any> {
+    return this.http.put<Transaction>(`${this.baseUrl}/${id}`, data);
+  }
+
+  deleteTransaction(id: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+}
