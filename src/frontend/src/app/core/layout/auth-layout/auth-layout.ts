fde6c784c91d9454077ce3000d78a1cbb571fe9c
Belleza411 <aevanazelleb@gmail.com>
2025-12-16 11:57:32 +1300

    chore: Added MainLayout and moved AuthLayout in core folder

---
+++ src/frontend/src/app/core/layout/auth-layout/auth-layout.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+
+@Component({
+  selector: 'app-auth-layout',
+  imports: [RouterOutlet],
+  templateUrl: './auth-layout.html',
+  styleUrl: './auth-layout.css',
+})
+export class AuthLayout {
+}
