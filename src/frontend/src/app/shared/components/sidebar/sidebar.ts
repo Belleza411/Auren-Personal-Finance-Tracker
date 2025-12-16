7dc114757e86c4e917d363bb3ed714f7627382e8
Belleza411 <aevanazelleb@gmail.com>
2025-12-16 19:10:21 +1300

    feat: Added logout functionality

--- src/frontend/src/app/shared/components/sidebar/sidebar.ts
+++ src/frontend/src/app/shared/components/sidebar/sidebar.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+import { AuthService } from '../../../features/auth/service/auth-service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,5 +10,22 @@ import { RouterLink } from "@angular/router";
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  private readonly authSer = inject(AuthService);
+  private destroyRef = inject(DestroyRef);
+  private readonly router = inject(Router);
 
+  error = signal<string | null>(null);
+
+  logout() {
+    this.authSer.logout()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: () => this.router.navigate(['/auth/sign-in']),
+        error: err => {
+          console.error("Something went wrong ", err);
+          this.error.set("Something went wrong. ");
+          this.router.navigate(['/auth/sign-in']);
+        }
+      })
+  }
 }
