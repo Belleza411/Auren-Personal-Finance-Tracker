255472beee950ec72d46f482bc2798b54a0aeb68
Belleza411 <aevanazelleb@gmail.com>
2025-12-16 19:11:03 +1300

    feat: Added transaction route in main-route, and added margin, and background color

--- src/frontend/src/app/core/routes/main-route.ts
+++ src/frontend/src/app/core/routes/main-route.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "../../features/dashboard/component/dashboard/dashboard";
+import { TransactionComponent } from "../../features/transactions/pages/transactions/transactions.component";
 
 export const mainRoutes: Routes = [
     {
         path: 'dashboard',
         component: DashboardComponent
+    },
+    {
+        path: 'transactions',
+        component: TransactionComponent
     }
 ]
