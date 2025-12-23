eefd99d9b7859ef478469987ba4a0e0a297f0789
Belleza411 <aevanazelleb@gmail.com>
2025-12-24 09:52:55 +1300

    feat: Added error messages and styling

--- src/frontend/src/app/features/transactions/components/transaction-form/transaction-form.ts
+++ src/frontend/src/app/features/transactions/components/transaction-form/transaction-form.ts
@@ -1,6 +1,6 @@
-import { Component, effect, input, output, signal } from '@angular/core';
+import { Component, computed, effect, input, output, signal } from '@angular/core';
 import { NewTransaction } from '../../models/transaction.model';
-import { Field, form } from '@angular/forms/signals';
+import { Field, FieldState, form, required, validate } from '@angular/forms/signals';
 import { Category } from '../../../categories/models/categories.model';
 import { TransactionTypeMap, PaymentTypeMap } from '../../constants/transaction-map';
 import { EnumSelect } from '../../../../shared/components/enum-select/enum-select';
@@ -12,7 +12,6 @@ import { EnumSelect } from '../../../../shared/components/enum-select/enum-selec
   styleUrl: './transaction-form.css',
 })
 export class TransactionForm {
-  isLoading = signal(false);
   isEdit = input(false);
   model = input.required<NewTransaction>();
   save = output<NewTransaction>();
@@ -21,8 +20,27 @@ export class TransactionForm {
   TransactionTypeMap = TransactionTypeMap;
   PaymentTypeMap = PaymentTypeMap;
 
+  isLoading = signal(false);
+
   private readonly modelSignal = signal({} as NewTransaction);
-  protected readonly transactionForm = form(this.modelSignal);
+  protected readonly transactionForm = form(this.modelSignal, schema => {
+    required(schema.name, { message: "Name is required"});
+    required(schema.amount, { message: 'Amount is required' });
+    required(schema.category, { message: "Category is required"});
+    required(schema.transactionType, { message: 'Transaction type is required' });
+    required(schema.paymentType, { message: 'Payment type is required' });
+
+    validate(schema.amount, ({ value }) => {
+      const amount = value();
+      if (amount <= 0) {
+        return {
+          kind: 'invalidAmount',
+          message: 'Amount must be greater than 0'
+        };
+      }
+      return null;
+    });
+  });
 
   constructor() {
     effect(() => {
@@ -39,4 +57,20 @@ export class TransactionForm {
   onCancel(): void {
     this.cancel.emit();
   }
+
+  protected fieldErrors = {
+    name: this.createErrorSignal(() => this.transactionForm.name()),
+    amount: this.createErrorSignal(() => this.transactionForm.amount()),
+    category: this.createErrorSignal(() => this.transactionForm.category()),
+    transactionType: this.createErrorSignal(() => this.transactionForm.transactionType()),
+    paymentType: this.createErrorSignal(() => this.transactionForm.paymentType())
+  }
+
+  private createErrorSignal(field: () => FieldState<string | number>) {
+    return computed(() => this.setShowError(field()));
+  };
+
+  private setShowError(field: FieldState<string | number>) {
+    return field.invalid() && field.touched();
+  };
 }
