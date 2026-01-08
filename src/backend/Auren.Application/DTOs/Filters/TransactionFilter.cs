533e842f92bd6eea73c4743e871a32ddc055a005
Belleza411 <aevanazelleb@gmail.com>
2026-01-08 18:44:33 +1300

    swap the date because it's confusing

--- src/backend/Auren.Application/DTOs/Filters/TransactionFilter.cs
+++ src/backend/Auren.Application/DTOs/Filters/TransactionFilter.cs
@@ -9,8 +9,8 @@ namespace Auren.Application.DTOs.Filters
         public TransactionType? TransactionType { get; set; }
         public decimal? MinAmount { get; set; }
 		public decimal? MaxAmount { get; set; }
-		public DateTime? EndDate { get; set; }
 		public DateTime? StartDate { get; set; }
+		public DateTime? EndDate { get; set; }
 		public IEnumerable<string>? Category { get; set; }
 		public PaymentType? PaymentType { get; set; }
     }
