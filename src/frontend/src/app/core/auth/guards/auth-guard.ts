231d776d959417693ac0b580f2768e6675eaa622
Belleza411 <aevanazelleb@gmail.com>
2026-02-16 21:40:55 +1300

    chore: Moved interceptor and guard to auth folder becasue they are both auth-related

--- src/frontend/src/app/core/auth/guards/auth-guard.ts
+++ src/frontend/src/app/core/auth/guards/auth-guard.ts
@@ -2,9 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { catchError, map, of } from 'rxjs';
-import { apiUrl } from '../../../environments/environment';
+import { apiUrl } from '../../../../environments/environment';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = () => {
   const baseUrl = `${apiUrl}/api/profile`;
   const http = inject(HttpClient);
   const router = inject(Router);
