0047166579427c42ec11d35c843a87ab8e72bc62
Belleza411 <aevanazelleb@gmail.com>
2026-04-09 22:13:38 +1200

    refactor: Update layout and styling for various components to improve responsiveness and consistency

--- src/frontend/src/app/shared/ui/navbar/navbar.component.ts
+++ src/frontend/src/app/shared/ui/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 import { ProfileComponent } from "../../../features/profile/profile/profile";
 import { NAVBAR_MENU } from './navbar-menu';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,35 @@ import { NAVBAR_MENU } from './navbar-menu';
   styleUrl: './navbar.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Navbar {
+export class Navbar implements OnInit {
+  private breakpointObserver = inject(BreakpointObserver);
   readonly sidebarItems = NAVBAR_MENU;
+  isMobile = signal(false);
+  isMenuOpen = signal(false);
+  isClosing = signal(false);
+  
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe(['(max-width: 640px)'])
+      .subscribe(result => {
+        this.isMobile.set(result.matches);
+        if(!this.isMobile) this.isMenuOpen.set(false);
+      })
+  }
+
+  toggleMenu() {
+    if(this.isMenuOpen() && !this.isClosing()) {
+      this.closeMenu();
+    } else {
+      this.isMenuOpen.set(true);
+    }
+  }
+
+  closeMenu() {
+    this.isClosing.set(true);
+    setTimeout(() => {
+      this.isMenuOpen.set(false);
+      this.isClosing.set(false);
+    }, 200); 
+  }
 }
--- src/frontend/src/app/shared/ui/summary-card/summary-card.ts
+++ src/frontend/src/app/shared/ui/summary-card/summary-card.ts
@@ -5,7 +5,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   templateUrl: './summary-card.html',
   styleUrl: './summary-card.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'bg-white border border-gray-300 rounded-[14px] flex flex-col px-[1.2rem] py-[1.1rem] shadow-lg'
+  }
 })
 export class SummaryCard {
 
