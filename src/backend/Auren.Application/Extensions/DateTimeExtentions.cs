9dfbd4a3b5a11d134ac12d950130c67ff8c4774d
Belleza411 <aevanazelleb@gmail.com>
2026-04-15 15:01:20 +1200

    refactor: Convert extension function into new c# extension method

--- src/backend/Auren.Application/Extensions/DateTimeExtentions.cs
+++ src/backend/Auren.Application/Extensions/DateTimeExtentions.cs
@@ -2,43 +2,46 @@
 {
 	public static class DateTimeExtentions
 	{
-        public static (DateTime start, DateTime end) GetLastMonthRange(this DateTime date)
+        extension(DateTime date)
         {
-            var firstDayOfCurrentMonth = new DateTime(date.Year, date.Month, 1);
-            var firstDayOfLastMonth = firstDayOfCurrentMonth.AddMonths(-1);
-            var lastDayOfLastMonth = firstDayOfCurrentMonth.AddDays(-1);
+            public (DateTime start, DateTime end) GetLastMonthRange()
+            {
+                var firstDayOfCurrentMonth = new DateTime(date.Year, date.Month, 1);
+                var firstDayOfLastMonth = firstDayOfCurrentMonth.AddMonths(-1);
+                var lastDayOfLastMonth = firstDayOfCurrentMonth.AddDays(-1);
 
-            return (firstDayOfLastMonth, lastDayOfLastMonth);
-        }
+                return (firstDayOfLastMonth, lastDayOfLastMonth);
+            }
 
-        public static (DateTime start, DateTime end) GetCurrentMonthRange(this DateTime date)
-        {
-            var firstDayOfCurrentMonth = new DateTime(date.Year, date.Month, 1);
-            return (firstDayOfCurrentMonth, date);
-        }
+            public (DateTime start, DateTime end) GetCurrentMonthRange()
+            {
+                var firstDayOfCurrentMonth = new DateTime(date.Year, date.Month, 1);
+                return (firstDayOfCurrentMonth, date);
+            }
 
-        public static (DateTime start, DateTime end) GetLast3MonthRange(this DateTime date)
-        {
-            var firstDayOfCurrentMonth = new DateTime(date.Year, date.Month, 1);
-            var firstDayOfLast3Months = firstDayOfCurrentMonth.AddMonths(-3);
-            var lastDayOfLast3Months = firstDayOfCurrentMonth.AddDays(-1);
+            public (DateTime start, DateTime end) GetLast3MonthRange()
+            {
+                var firstDayOfCurrentMonth = new DateTime(date.Year, date.Month, 1);
+                var firstDayOfLast3Months = firstDayOfCurrentMonth.AddMonths(-3);
+                var lastDayOfLast3Months = firstDayOfCurrentMonth.AddDays(-1);
 
-            return (firstDayOfLast3Months, lastDayOfLast3Months);
-        }
+                return (firstDayOfLast3Months, lastDayOfLast3Months);
+            }
 
-        public static (DateTime start, DateTime end) GetLast6MonthRange(this DateTime date)
-        {
-            var firstDayOfCurrentMonth = new DateTime(date.Year, date.Month, 1);
-            var firstDayOfLast6Months = firstDayOfCurrentMonth.AddMonths(-6);
-            var lastDayOfLast6Months = firstDayOfCurrentMonth.AddDays(-1);
+            public (DateTime start, DateTime end) GetLast6MonthRange()
+            {
+                var firstDayOfCurrentMonth = new DateTime(date.Year, date.Month, 1);
+                var firstDayOfLast6Months = firstDayOfCurrentMonth.AddMonths(-6);
+                var lastDayOfLast6Months = firstDayOfCurrentMonth.AddDays(-1);
 
-            return (firstDayOfLast6Months, lastDayOfLast6Months);
-        }
+                return (firstDayOfLast6Months, lastDayOfLast6Months);
+            }
 
-        public static (DateTime start, DateTime end) GetThisYearRange(this DateTime date)
-        {
-            var firstDayOfThisYear = new DateTime(date.Year, 1, 1);
-            return (firstDayOfThisYear, date);
+            public (DateTime start, DateTime end) GetThisYearRange()
+            {
+                var firstDayOfThisYear = new DateTime(date.Year, 1, 1);
+                return (firstDayOfThisYear, date);
+            }
         }
     }
 }
--- src/backend/Auren.Application/Extensions/TimePeriodExtension.cs
+++ src/backend/Auren.Application/Extensions/TimePeriodExtension.cs
@@ -7,32 +7,35 @@ namespace Auren.Application.Extensions
 {
     public static class TimePeriodExtension
     {
-        public static (DateTime start, DateTime end) GetTimePeriodRange(this TimePeriod? timePeriod)
+        extension(TimePeriod? timePeriod)
         {
-            return timePeriod switch
+            public (DateTime start, DateTime end) GetTimePeriodRange()
             {
-                TimePeriod.Last3Months => DateTime.Today.GetLast3MonthRange(),
-                TimePeriod.Last6Months => DateTime.Today.GetLast6MonthRange(),
-                TimePeriod.ThisYear => DateTime.Today.GetThisYearRange(),
-                TimePeriod.LastMonth => DateTime.Today.GetLastMonthRange(),
-                TimePeriod.ThisMonth => DateTime.Today.GetCurrentMonthRange(),
-                TimePeriod.AllTime => (DateTime.MinValue, DateTime.Today),
-                _ => (DateTime.MinValue, DateTime.Today)
-            };
-        }
+                return timePeriod switch
+                {
+                    TimePeriod.Last3Months => DateTime.Today.GetLast3MonthRange(),
+                    TimePeriod.Last6Months => DateTime.Today.GetLast6MonthRange(),
+                    TimePeriod.ThisYear => DateTime.Today.GetThisYearRange(),
+                    TimePeriod.LastMonth => DateTime.Today.GetLastMonthRange(),
+                    TimePeriod.ThisMonth => DateTime.Today.GetCurrentMonthRange(),
+                    TimePeriod.AllTime => (DateTime.MinValue, DateTime.Today),
+                    _ => (DateTime.MinValue, DateTime.Today)
+                };
+            }
 
-        public static (DateTime start, DateTime end) GetPreviousTimePeriodRange(this TimePeriod? timePeriod)
-        {
-            return timePeriod switch
+            public (DateTime start, DateTime end) GetPreviousTimePeriodRange()
             {
-                TimePeriod.Last3Months => DateTime.Today.AddMonths(-3).GetLast3MonthRange(),
-                TimePeriod.Last6Months => DateTime.Today.AddMonths(-6).GetLast6MonthRange(),
-                TimePeriod.ThisYear => DateTime.Today.AddYears(-1).GetThisYearRange(),
-                TimePeriod.LastMonth => DateTime.Today.AddMonths(-2).GetLastMonthRange(),
-                TimePeriod.ThisMonth => DateTime.Today.GetLastMonthRange(),
-                TimePeriod.AllTime => (DateTime.MinValue, DateTime.Today),
-                _ => (DateTime.MinValue, DateTime.Today)
-            };
+                return timePeriod switch
+                {
+                    TimePeriod.Last3Months => DateTime.Today.AddMonths(-3).GetLast3MonthRange(),
+                    TimePeriod.Last6Months => DateTime.Today.AddMonths(-6).GetLast6MonthRange(),
+                    TimePeriod.ThisYear => DateTime.Today.AddYears(-1).GetThisYearRange(),
+                    TimePeriod.LastMonth => DateTime.Today.AddMonths(-2).GetLastMonthRange(),
+                    TimePeriod.ThisMonth => DateTime.Today.GetLastMonthRange(),
+                    TimePeriod.AllTime => (DateTime.MinValue, DateTime.Today),
+                    _ => (DateTime.MinValue, DateTime.Today)
+                };
+            }
         }
     }
 }
