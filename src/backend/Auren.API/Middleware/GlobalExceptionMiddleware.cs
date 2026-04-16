bbf60509c9abab58ef6ef3afa864677f0a7d31da
Belleza411 <aevanazelleb@gmail.com>
2026-04-16 20:45:46 +1200

    refactor: Refactored to catch OperationCancelledException to handle the connection rather than to make the system stopped

--- src/backend/Auren.API/Middleware/GlobalExceptionMiddleware.cs
+++ src/backend/Auren.API/Middleware/GlobalExceptionMiddleware.cs
@@ -13,6 +13,27 @@ namespace Auren.API.Middleware
 			Exception exception,
 			CancellationToken cancellationToken)
 		{
+            if(exception is OperationCanceledException)
+            {
+                logger.LogInformation("Request was cancelled (client disconnected or hot reload).");
+
+                context.Response.StatusCode = 499;
+
+                return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+                {
+                    HttpContext = context,
+                    Exception = exception,
+                    ProblemDetails = new ProblemDetails
+                    {
+                        Type = nameof(OperationCanceledException),
+                        Title = "Request Cancelled",
+                        Detail = "The request was cancelled by the client.",
+                        Status = StatusCodes.Status499ClientClosedRequest,
+                        Instance = context.Request.GetDisplayUrl()
+                    }
+                });
+            }
+
             logger.LogError(exception, "Something went wrong");
 
             return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
@@ -25,7 +46,7 @@ namespace Auren.API.Middleware
                     Title = "An error occured",
                     Detail = exception.Message,
                     Status = StatusCodes.Status500InternalServerError,
-                    Instance = context.Request.Path
+                    Instance = context.Request.GetDisplayUrl()
                 }
             });
 		}
